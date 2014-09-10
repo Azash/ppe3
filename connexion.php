0685f768bf66c2703b91a42cf519d52e02834ed5
guigui94370 <guillaume.boudy@gmail.com>
2014-09-10 12:14:45 +0200

    encodage de test.php + modif connexion.php

--- connexion.php
+++ connexion.php
@@ -1,17 +1,17 @@
 <?php
-$serveur="localhost";
-$base="sos_partenaire";
-$utilisateur="root";
-$mot_passe="";
 
-$db = mysql_connect($serveur,$utilisateur,$mot_passe);
-
-mysql_select_db($base,$db);
-
+@$connexion = mysql_connect("localhost","root","") or exit(mysql_error());
+@mysql_select_db("sos_partenaire") or exit(mysql_error());
 ?>
 
 <?php
+//$serveur="localhost";
+//$base="sos_partenaire";
+//$utilisateur="root";
+//$mot_passe="";
 
-//@$connexion = mysql_connect("localhost","root","") or exit(mysql_error());
-//@mysql_select_db("sos_partenaire") or exit(mysql_error());
+//$db = mysql_connect($serveur,$utilisateur,$mot_passe);
+
+//mysql_select_db($base,$db);
+
 ?>
