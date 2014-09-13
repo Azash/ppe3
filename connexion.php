93d87a48d4d7c40b17ae3387934b5cfa22dc1871
theBlatte <estelle.andree@hotmail.fr>
2014-09-13 16:40:50 +0200

    pas tenir compte //test

--- connexion.php
+++ connexion.php
@@ -1,17 +1,17 @@
 <?php
-$serveur="localhost";
-$base="sos_partenaire";
-$utilisateur="root";
-$mot_passe="";
+//$serveur="localhost";
+//$base="sos_partenaire";
+//$utilisateur="root";
+//$mot_passe="";
 
-$db = mysql_connect($serveur,$utilisateur,$mot_passe);
+//$db = mysql_connect($serveur,$utilisateur,$mot_passe);
 
-mysql_select_db($base,$db);
+//mysql_select_db($base,$db);
 
 ?>
 
 <?php
 
-//@$connexion = mysql_connect("localhost","root","") or exit(mysql_error());
-//@mysql_select_db("sos_partenaire") or exit(mysql_error());
+@$connexion = mysql_connect("localhost","root","") or exit(mysql_error());
+@mysql_select_db("sos_partenaire") or exit(mysql_error());
 ?>
