2d618a58cf8a6e6c46f294fa8239c27552449c18
guigui94370 <guillaume.boudy@gmail.com>
2014-10-06 15:25:27 +0200

    modif 1 fichier

--- ConnexionReussie.php
+++ ConnexionReussie.php
@@ -19,19 +19,7 @@
 	<body>
 		<div id="conteneur">
 <!--HEADER-->		
-			<div id="entete">
-				<div id="menu_entete">
-					<a href="/forum/" id="ctl00_menu_Entete_menu_forum" class="dernier">Forums</a>
-					<a href="/conseils/" id="ctl00_menu_Entete_menu_conseils">Conseils</a>
-					<a href="/annuaire/" id="ctl00_menu_Entete_menu_annuaire">Annuaire</a>
-				
-					<a href="activites.php" id="ctl00_menu_Entete_menu_activites">Activités</a>
-					<a href="/partenaire/" id="ctl00_menu_Entete_menu_membres">Membres</a>
-					<a href="/partenaires/" id="ctl00_menu_Entete_menu_partenaires">Partenaires</a>
-					<a href="/avantages.aspx" id="ctl00_menu_Entete_menu_mycleec">Mycleec</a>
-					<div class="spacer"></div>
-				</div>
-			</div>
+			<?php getHeader(); ?>
 <!--FIN HEADER-->
 <!--CONTENU-->			
 			<div id="contenu_annexe">
@@ -91,13 +79,7 @@
 			</div>
 <!--FIN CONTENU-->			
 <!--FOOTER-->			
-			<div id="pied">
-				<a href="/site-tour.aspx">Aide</a> |
-				<a href="/nos-partenaires/">Nos Partenaires</a> |
-				<a href="/infos/presse.aspx">Presse</a> |
-				<a href="/infos/conditionsdutilisation.aspx">Conditions d'utilisation</a> |
-				<a href="/infos/contact.aspx">Contact</a>
-			</div>
+			<?php getFooter() ?>
 <!--FIN FOOTER-->			
 		</div>
 	</body>
