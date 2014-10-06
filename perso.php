b455c37e46cb45369a53c9ddcd137241ba4b0925
Azash <azashh@gmail.com>
2014-10-06 14:29:03 +0200

    C'est du chinois ?

---
+++ perso.php
@@ -0,0 +1,63 @@
+<?php 
+	//Pour se connecter à la base de données
+	include("connexion.php");
+?>
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+	<title>SOS partenaires</title>
+	<link rel="stylesheet" href="style.css" type="text/css" charset="utf-8">
+	<!--<script style="" type="text/javascript" src="/global/jquery-1.11.0.min.js"></script>-->
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta name="Author" content="SOSpartenaires">
+	<meta name="description" content="Sur SOS partenaires trouvez des activités près de chez vous, Organisez vos sports et loisirs avec vos amis, Trouvez de nouveaux partenaires.">
+	<meta name="Keywords" content="SOS partenaires, sports et loisirs, sport, loisir, coach, reseau, social, bien, etre, bien-etre, loisirs, sports, communaute, social">
+
+	<!--<link rel="shortcut icon" href="/favicon.ico">-->
+	<body>
+		<div id="conteneur">
+<!--HEADER-->		
+			<div id="entete">
+				<div id="menu_entete">
+					<a href="/forum/" id="ctl00_menu_Entete_menu_forum" class="dernier">Forums</a>
+					<a href="/conseils/" id="ctl00_menu_Entete_menu_conseils">Conseils</a>
+					<a href="/annuaire/" id="ctl00_menu_Entete_menu_annuaire">Annuaire</a>
+				
+					<a href="activites.php" id="ctl00_menu_Entete_menu_activites">Activités</a>
+					<a href="/partenaire/" id="ctl00_menu_Entete_menu_membres">Membres</a>
+					<a href="/partenaires/" id="ctl00_menu_Entete_menu_partenaires">Partenaires</a>
+					<a href="/avantages.aspx" id="ctl00_menu_Entete_menu_mycleec">Mycleec</a>
+					<div class="spacer"></div>
+				</div>
+			</div>
+<!--FIN HEADER-->
+<!--CONTENU-->			
+			<div id="contenu_annexe">
+
+
+<!--Fin de Zone Estelle-->
+				
+				<!--CORRESPOND A UNE BOITE SMALL
+				<div class="boitegrise_305">
+					<h2></h2>
+					<div class="finboite"></div>
+				</div>-->
+				<!--CORRESPON A LA BOITE MEDIUM-->
+				<div class="boitegrise_466 sans_marge_gauche">
+					<h2></h2>
+					<div class="finboite"></div>
+				</div>
+				
+				<div class="spacer"></div>
+			</div>
+<!--FIN CONTENU-->			
+<!--FOOTER-->			
+			<div id="pied">
+				<a href="/site-tour.aspx">Aide</a> |
+				<a href="/nos-partenaires/">Nos Partenaires</a> |
+				<a href="/infos/presse.aspx">Presse</a> |
+				<a href="/infos/conditionsdutilisation.aspx">Conditions d'utilisation</a> |
+				<a href="/infos/contact.aspx">Contact</a>
+			</div>
+<!--FIN FOOTER-->			
+		</div>
+	</body>
+</html>
