8fd008ae07df06597f3df261a459f8da6d331739
guigui94370 <guillaume.boudy@gmail.com>
2014-10-08 12:21:07 +0200

    quelques modif

--- Desinscription.php
+++ Desinscription.php
@@ -1,11 +1,45 @@
 <?php
-     session_start(); 
-     @$connexion = mysql_connect("localhost","root","") or exit(mysql_error()); //why @?
-     @mysql_select_db("sos_partenaire") or exit(mysql_error());
+     session_start();
+		
+	$message = '';
+
+	if(isset($_POST['submit']))
+	{
+		$email = htmlspecialchars(trim($_POST['email']));
+		$mdp = htmlspecialchars(trim($_POST['mdp']));
 	
-     
-     mysql_query("DELETE FROM users WHERE email = '{$_SESSION['email']}'");
+		if(empty($email))
+		{
+			echo "Veuillez saisir votre email<br/>";
+		}
+		else if(empty($mdp))
+		{
+			echo "Veuillez saisir votre mot de passe";
+		}
+		else{
+
+		
+
+		$mdp = sha1($mdp);
+		
+		$Connexion = mysql_query("SELECT email, id FROM users WHERE email='$email' AND mdp='$mdp'");
+		$select = mysql_fetch_array($Connexion, MYSQL_ASSOC);
+		
+		if(count($select) > 0)
+		{
+			session_regenerate_id(true);
+			
+			$_SESSION['email'] = $select['email'];
+			$_SESSION['id'] = $select['id'];
+			header('location:desinscription.php'); //(=connexion réussie)
+		}
+		else 
+
+			$message= "Email ou mot de passe incorrect";
+		}
+	}
+	 
+     $del = mysql_query("DELETE FROM users WHERE email = '{$_SESSION['email']}'");
      
      header("Location:Deconnexion.php")
-     //header("Location:ConnexionReussie.php")
 ?>
--- connexion.php
+++ connexion.php
@@ -1,4 +1,5 @@
-<?php
+<?php  /* CONNEXION */
+
 	include('connexionbdd.php');
 	session_start();
 	$message = '';
@@ -42,48 +43,3 @@
 		}
 	}
 ?>
-
-<?php
-	mysql_query("set names 'utf8'");
-	
-	if(isset($_POST['submit']))
-	{
-		$nom = htmlspecialchars($_POST["nom"]);
-		$prenom = htmlspecialchars($_POST["prenom"]);
-		//$birth = htmlspecialchars($_POST["birth"]);
-		$jourNaissance = intval($_POST['jourNaissance']);
-		$moisNaissance = intval($_POST['moisNaissance']);
-		$anneeNaissance = intval($_POST['anneeNaissance']);
-		$mdp = htmlspecialchars($_POST["mdp"]);
-		$re_mdp = htmlspecialchars(trim($_POST['re_mdp']));
-		$sexe = htmlspecialchars($_POST["sexe"]);
-		$email = htmlspecialchars($_POST["email"]);
-
-
-		if($nom && $prenom && $mdp && $re_mdp && $sexe && $email)
-		{
-			if(strlen($nom)>=2)
-			{
-				if(strlen($prenom)>=2)
-				{
-					if($mdp == $re_mdp)
-					{
-						$mdp = SHA1($mdp);   //cryptage du mdp
-							
-						//include_once 'includes/connexionbdd.inc.php';
-						//$query = mysql_query("INSERT INTO users VALUES('', '$nom', '$prenom', '" . $anneeNaissance . "-" . $moisNaissance . "-" . $jourNaissance . "-1', $sexe', '$email', '$mdp')");
-						$date = $anneeNaissance . "-" . $moisNaissance . "-" . $jourNaissance; 
-						$sql = "INSERT INTO users (id, nom, prenom, birth, mdp, sexe, email) VALUES('null', '$nom', '$prenom', '$date', '$mdp', '$sexe', '$email')";
-						echo $sql;
-						$res = mysql_query($sql);
-						if(!$res) echo mysql_error();
-						//mysql_query("INSERT INTO users VALUES('', '$nom', '$prenom', '" . $anneeNaissance . "-" . $moisNaissance . "-" . $jourNaissance . "', $sexe', '$email', '$mdp')");
-						die('Formulaire bien rempli! Vous allez maintenant reçevoir un mail avec un mot de passe de confirmation. Vous pourrez ensuite choisir votre propre mot de passe pour vous connecter.<a href="Connexion.php">Continuer mon inscription</a>');
-							
-					}else echo "Les mots de passe ne sont pas identiques";
-				}else echo "Veuillez rentrer un prenom de + de 2 caractères";
-			}else echo "Veuillez rentrer un nom de + de 2 caractères";
-		}else echo "Veuillez saisir tous les champs!";
-	}
-?>
-
